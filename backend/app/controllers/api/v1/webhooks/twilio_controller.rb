5d02c9243216a24c9457d0347523b1b27c7829cf
Arno Khachatourian <arno.kha@gmail.com>
2025-10-31 09:44:01 -0400

    logging cleanup

--- backend/app/controllers/api/v1/webhooks/twilio_controller.rb
+++ backend/app/controllers/api/v1/webhooks/twilio_controller.rb
@@ -9,21 +9,16 @@ module Api
           message_sid = params['MessageSid']
           message_status = params['MessageStatus']
 
-          Rails.logger.info("Twilio webhook received - SID: #{message_sid}, Status: #{message_status}")
-
           message = Message.find_by(sid: message_sid)
 
           if message
             message.update!(status: message_status)
-            Rails.logger.info("Message status updated: #{message.id} -> #{message_status}")
             head :ok
           else
-            Rails.logger.warn("Message not found for SID: #{message_sid}")
             head :not_found
           end
         rescue => e
           Rails.logger.error("Twilio webhook error: #{e.class} - #{e.message}")
-          Rails.logger.error(e.backtrace.join("\n"))
           head :internal_server_error
         end
 
@@ -37,7 +32,6 @@ module Api
           post_params = request.request_parameters
 
           unless validator.validate(url, post_params, signature)
-            Rails.logger.warn("Invalid Twilio signature for webhook request")
             head :forbidden
           end
         end
