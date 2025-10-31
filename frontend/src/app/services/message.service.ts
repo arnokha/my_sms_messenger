0cddc8ff278449f297c57a75e6ac43cbb4157df3
Arno Khachatourian <arno.kha@gmail.com>
2025-10-31 06:18:18 -0400

    netlify toml, cors allow all for demo, api url change

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -7,12 +7,11 @@ import { Message } from '../models/message.model';
   providedIn: 'root'
 })
 export class MessageService {
-  private apiUrl = 'http://localhost:3000/api/v1/messages';
+  private apiUrl = 'https://my-sms-messenger-9lff.onrender.com/api/v1/messages';
 
   constructor(private http: HttpClient) {}
 
   sendMessage(to: string, body: string, session_id: string): Observable<Message> {
-    
     return this.http.post<Message>(this.apiUrl, { message: { to, body, session_id } });
   }
 
