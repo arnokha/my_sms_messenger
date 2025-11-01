4cd2ebe269474ffaa09b7f4d46090b923d9d5589
Arno Khachatourian <arno.kha@gmail.com>
2025-11-01 05:00:03 -0400

    stubs and routes for new frontend models, services, components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 import { MessagesComponent } from './messages/messages.component';
 
 export const routes: Routes = [
-    { path: '', component: MessagesComponent }
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'messages', component: MessagesComponent }
 ];
